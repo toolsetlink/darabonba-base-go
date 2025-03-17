8ae3c3cb25345dfac87bc0bb2afc585d79f7b97b
songang <songang@le.com>
2025-03-17 14:56:24 +0800

    init

---
+++ client/client.go
@@ -0,0 +1,43 @@
+package client
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"strings"
+	"time"
+)
+
+func TimeRFC3339() (_result string) {
+	timestamp := time.Now().Format(time.RFC3339)
+	return timestamp
+}
+
+// 生成随机Nonce (16位十六进制)
+func GenerateNonce() string {
+	bytes := make([]byte, 8)
+	if _, err := rand.Read(bytes); err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(bytes)
+}
+
+// 生成请求签名
+func GenerateSignature(body, nonce, secretKey, timestamp, uri string) string {
+	var parts []string
+
+	if body != "" {
+		parts = append(parts, "body="+body)
+	}
+
+	parts = append(parts,
+		"nonce="+nonce,
+		"secretKey="+secretKey,
+		"timestamp="+timestamp,
+		"url="+uri,
+	)
+
+	signStr := strings.Join(parts, "&")
+	hash := md5.Sum([]byte(signStr))
+	return hex.EncodeToString(hash[:])
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/toolsetlink/darabonba-base-go
+
+go 1.23.4
+
+require (
+)
